aa0f146ea5bc9c0c3068f9277f9f88de1ac89dda
kike4815 <e.pedros.sanchez@gmail.com>
2020-03-27 02:35:18 +0100

    add Map Component with Markers

---
+++ staff/enric-pedros/badabici/badabici-app/src/components/Map.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+
+import "./Map.css";
+
+
+export default function App() {
+
+const position = [41.432184, 2.218863]
+    return (
+        <Map center={[41.432184, 2.218863]} zoom={15}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            <Marker position={position}>
+                <Popup>BADABICI SPORT <br/> tlf. 933811943 </Popup>
+            </Marker>
+        </Map>
+    );
+}
