0d9601a9686b9c2ee3d20049d7e0708ab0167130
kike4815 <e.pedros.sanchez@gmail.com>
2020-03-28 16:41:33 +0100

    ordered coverage and reactor template sass

--- staff/enric-pedros/badabici/badabici-api/logic/ordered.spec.js
+++ staff/enric-pedros/badabici/badabici-api/logic/ordered.spec.js
@@ -7,7 +7,7 @@ const buyit = require('./ordered')
 const { ContentError, NotAllowedError } = require('badabici-errors')
 const { env: { TEST_MONGODB_URL } } = process
 
-describe.only('ordered', () => {
+describe('ordered', () => {
     before(() =>
         mongoose.connect(TEST_MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
             .then(() => Promise.all([User.deleteMany(), Product.deleteMany()]))
